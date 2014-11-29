a0e76f02d920a7c16871916a00a861115e823f7e
Quentin Vecchio <vecchioquentin@hotmail.fr>
2014-11-29 15:16:37 +0100

    Modification classe Ellipse

--- Client_Simple/Sources/Ellipse.cpp
+++ Client_Simple/Sources/Ellipse.cpp
@@ -1,28 +1,44 @@
 #include "../Headers/Ellipse.h"
 
-Ellipse::Ellipse(const Point *p, const Point *p2,const float r, const Couleur::Couleurs c):Cercle(p,r,c)
+Ellipse::Ellipse(const Point *p,const float r, const float h, const Couleur::Couleurs c):Cercle(p,r,c)
 {
-    if(r < 0){
+    if(r < 0 || h < 0)
+    {
         ExceptionGeo2D *e = new ExceptionGeo2D("Erreur, le rayon d'une ellipse doit etre strictement superieur a 0");
         cout << e->get_erreur() << endl;
         exit(1);
     }
-    else{
-        this->p2 = p2->copy();
+    else
+    {
+        this->hauteur = h;
     }
 }
 
 Ellipse::Ellipse(const Ellipse& e):Cercle(e.getP1(), e.getRayon(), e.getC())
 {
-    this->p2 = e.getP2();
+    this->hauteur = e.getHauteur();
 }
 
-Point *Ellipse::getP2()const{
-    return this->p2;
+Ellipse::~Ellipse()
+{
+
 }
 
-void Ellipse::setP2(const Point *p){
-    this->p2 = p->copy();
+float *Ellipse::getHauteur()const{
+    return this->h;
+}
+
+void Ellipse::setHauteur(const float h){
+    if(h < 0)
+    {
+        ExceptionGeo2D *e = new ExceptionGeo2D("Erreur, le rayon d'une ellipse doit etre strictement superieur a 0");
+        cout << e->get_erreur() << endl;
+        exit(1);
+    }
+    else
+    {
+        this->hauteur = h;
+    }
 }
 
 float Ellipse::getAire()const{
@@ -98,12 +114,16 @@ QDomElement Ellipse::toXml(QDomDocument * dom) const
     QDomText r = dom->createTextNode(a);
     rayon.appendChild(r);
     nom.appendChild(rayon);
+    //Création de la balise hauteur
+    QDomElement hauteur = dom->createElement("hauteur");
+    QString a;
+    a.setNum(this->getHauteur());
+    QDomText h = dom->createTextNode(a);
+    hauteur.appendChild(h);
+    nom.appendChild(hauteur);
     //Création de la balise point
     QDomElement point = this->getP1()->toXml(dom);
     nom.appendChild(point);
-    //Création de la balise point
-    QDomElement point2 = this->getP2()->toXml(dom);
-    nom.appendChild(point2);
     //Création de la balise couleur
     QDomElement couleur = dom->createElement("couleur");
     QDomText c = dom->createTextNode(Couleur::getCouleur(this->getC()));
@@ -119,7 +139,7 @@ Ellipse* Ellipse::copy() const
 
 void Ellipse::afficher(ostream& flux) const
 {
-    flux << "Ellipse[ Point[ x = " << this->getP1()->getX() << ", y = " << this->getP1()->getY() << "], Point[ x = " << this->getP2()->getX() << ", y = " << this->getP2()->getY() << "] " << " Rayon = " << this->getRayon() << ", couleur = " << Couleur::getCouleur(this->getC()).toStdString() << "] " << endl;
+    flux << "Ellipse[ Point[ x = " << this->getP1()->getX() << ", y = " << this->getP1()->getY() << "]" << " Rayon = " << this->getRayon() << " Hauteur = " << this->getHauteur() << ", couleur = " << Couleur::getCouleur(this->getC()).toStdString() << "] " << endl;
 }
 
 ostream& operator <<(ostream& flux, const Ellipse& c)
@@ -127,8 +147,3 @@ ostream& operator <<(ostream& flux, const Ellipse& c)
     c.afficher(flux);
     return flux;
 }
-
-Ellipse::~Ellipse(){
-    delete this->getP2();
-}
-
